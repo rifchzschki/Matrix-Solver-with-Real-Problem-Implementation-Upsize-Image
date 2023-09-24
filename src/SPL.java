b86d2af1bb2eee3ec77c7fa3a6afb36464f5e9b2
OrdinaryAzazel <19622052@mahasiswa.itb.ac.id>
2023-09-24 16:23:44 +0700

    fix bug -13522120

--- src/SPL.java
+++ src/SPL.java
@@ -1,146 +1,136 @@
-package src;
-import java.util.Scanner;
+// package src;
+// import java.util.Scanner;
 
-public class SPL{
-    public int baris,kolom; //m itu baris,n itu kolom
-    public double[][] matrix;
-    public double[] b;
-    public double[] array;
-    public double[][] augmentedMatrix;
-    Scanner scanner = new Scanner(System.in);
-    public void readMatrix()
-    {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Jumlah Baris : ");//m
-        baris = scanner.nextInt();
-        System.out.println("Jumlah Kolom : ");//n
-        kolom = scanner.nextInt();
-        matrix = new double[baris][kolom]; //a[i][j]
-        array = new double[baris]; //b[i]
-        for(int i = 0; i < baris; i++)
-        {
-            for(int j = 0; j < kolom; j++)
-            {   
-                System.out.println("Isi elemen a ke [" + (i) + "]" +"[" + (j) + "] : ");
-                double elemen = scanner.nextDouble();
-                matrix[i][j] = elemen;//koef a[i][j]
+// public class SPL{
+//     public int baris,kolom; //m itu baris,n itu kolom
+//     public double[][] matrix;
+//     public double[] b;
+//     public double[] array;
+//     public double[][] augmentedMatrix;
+//     Scanner scanner = new Scanner(System.in);
+//     // public void readMatrix()
+//     // {
+//     //     Scanner scanner = new Scanner(System.in);
+//     //     System.out.println("Jumlah Baris : ");//m
+//     //     baris = scanner.nextInt();
+//     //     System.out.println("Jumlah Kolom : ");//n
+//     //     kolom = scanner.nextInt();
+//     //     matrix = new double[baris][kolom]; //a[i][j]
+//     //     array = new double[baris]; //b[i]
+//     //     for(int i = 0; i < baris; i++)
+//     //     {
+//     //         for(int j = 0; j < kolom; j++)
+//     //         {   
+//     //             System.out.println("Isi elemen a ke [" + (i) + "]" +"[" + (j) + "] : ");
+//     //             double elemen = scanner.nextDouble();
+//     //             matrix[i][j] = elemen;//koef a[i][j]
                 
-            }
-            System.out.println("Isi elemen b ke [" + (i) + "] : ");
-            double elemen = scanner.nextDouble(); //elemen b[i]
-            array[i]=elemen;
-            // scanner.close();
-        }
-        // return matrix;
-    }
+//     //         }
+//     //         System.out.println("Isi elemen b ke [" + (i) + "] : ");
+//     //         double elemen = scanner.nextDouble(); //elemen b[i]
+//     //         array[i]=elemen;
+//     //         // scanner.close();
+//     //     }
+//     //     // return matrix;
+//     // }
 
-    public double[][] createAugmentedMatrix(double[][] a, double[] b)
-    {
-        augmentedMatrix = new double[baris][kolom+1];
+//     // public double[][] createAugmentedMatrix(double[][] a, double[] b)
+//     // {
+//     //     augmentedMatrix = new double[baris][kolom+1];
         
         
-        for(int i = 0; i < baris; i++)
-        {
-            for(int j = 0; j < kolom+1; j++)
-            {   
-                if (j == kolom)
-                {
-                    augmentedMatrix[i][j] = b[i];
-                }
-                else
-                {
-                augmentedMatrix[i][j] = a[i][j];
-                }
-            }
-        }
+//     //     for(int i = 0; i < baris; i++)
+//     //     {
+//     //         for(int j = 0; j < kolom+1; j++)
+//     //         {   
+//     //             if (j == kolom)
+//     //             {
+//     //                 augmentedMatrix[i][j] = b[i];
+//     //             }
+//     //             else
+//     //             {
+//     //             augmentedMatrix[i][j] = a[i][j];
+//     //             }
+//     //         }
+//     //     }
 
-        return augmentedMatrix;
-    }
+//     //     return augmentedMatrix;
+//     // }
 
-    public double[][] Gauss()
-    {
+//     public double[][] Gauss()
+//     {
 
         
-        readMatrix();
-        createAugmentedMatrix(matrix, array);
+//         readMatrix();
+//         createAugmentedMatrix(matrix, array);
 
-        // Eliminasi Gauss
-        for (int i = 0; i < baris; i++) {
-            for (int j = i + 1; j < kolom; j++) {
-                double ratio = augmentedMatrix[j][i] / augmentedMatrix[i][i];
-                for (int k = 0; k < kolom + 1; k++) {
-                    augmentedMatrix[j][k] -= ratio * augmentedMatrix[i][k];
-                }
-            }
-        }
-        printMatrix(augmentedMatrix,baris,kolom+1);
+//         // Eliminasi Gauss
+//         for (int i = 0; i < baris; i++) {
+//             for (int j = i + 1; j < kolom; j++) {
+//                 double ratio = augmentedMatrix[j][i] / augmentedMatrix[i][i];
+//                 for (int k = 0; k < kolom + 1; k++) {
+//                     augmentedMatrix[j][k] -= ratio * augmentedMatrix[i][k];
+//                 }
+//             }
+//         }
+//         printMatrix(augmentedMatrix,baris,kolom+1);
 
-        // Penyelesaian balik (back substitution)
-        double[] solusi = new double[baris];
-        for (int i = baris - 1; i >= 0; i--) {
-            solusi[i] = augmentedMatrix[i][baris];
-            for (int j = i + 1; j < baris; j++) {
-                solusi[i] -= augmentedMatrix[i][j] * solusi[j];
-            }
-            solusi[i] /= augmentedMatrix[i][i];
-        }
+//         // Penyelesaian balik (back substitution)
+//         double[] solusi = new double[baris];
+//         for (int i = baris - 1; i >= 0; i--) {
+//             solusi[i] = augmentedMatrix[i][baris];
+//             for (int j = i + 1; j < baris; j++) {
+//                 solusi[i] -= augmentedMatrix[i][j] * solusi[j];
+//             }
+//             solusi[i] /= augmentedMatrix[i][i];
+//         }
 
-        System.out.println("Hasil setelah eliminasi Gauss:");
-        for (int i = 0; i < baris; i++) {
-            System.out.println("x" + (i + 1) + " = " + solusi[i]);
-        }
-        return augmentedMatrix;
-    }
-
-
+//         System.out.println("Hasil setelah eliminasi Gauss:");
+//         for (int i = 0; i < baris; i++) {
+//             System.out.println("x" + (i + 1) + " = " + solusi[i]);
+//         }
+//         return augmentedMatrix;
+//     }
 
 
-     public static void main(String args[])
-    {
-        SPL testSpl = new SPL();
-        testSpl.Gauss();
 
-        // readMatrix();
-        // printMatrix(matrix,3,3);
 
-    }
+//      public static void main(String args[])
+//     {
+//         SPL testSpl = new SPL();
+//         testSpl.Gauss();
+
+//         // readMatrix();
+//         // printMatrix(matrix,3,3);
+
+//     }
 
    
     
     
-    public void printMatrix(double[][] matrix,int baris, int kolom)
-    {
-        for(int i = 0; i < baris; i++)
-        {
-            
-            for(int j = 0; j < kolom; j++)
-            {   
-               System.out.format(" %f ",matrix[i][j]);
-            }
-            
-            System.out.println();
-        }
-        // static void Gaus(String args[]) 
-    }
+//     publ
+    
+//         // static void Gaus(String args[]) 
+//     }
 
-    // public double[][] obe(double[][] matrix){
-    //     static hasil[][];
-    //     hasil
+//     // public double[][] obe(double[][] matrix){
+//     //     static hasil[][];
+//     //     hasil
 
 
 
-    //     return hasil;
-    // }
+//     //     return hasil;
+//     // }
 
     
-}
+// }
 
-// . Library tersebut berisi fungsi-fungsi seperti eliminasi Gauss,
-// eliminasi Gauss-Jordan,
-//     menentukan balikan matrix,
-//     menghitung determinan, kaidah
+// // . Library tersebut berisi fungsi-fungsi seperti eliminasi Gauss,
+// // eliminasi Gauss-Jordan,
+// //     menentukan balikan matrix,
+// //     menghitung determinan, kaidah
 
-// Cramer (kaidah Cramer khusus untuk SPL dengan n peubah dan n persamaan).
+// // Cramer (kaidah Cramer khusus untuk SPL dengan n peubah dan n persamaan).
 
     
 
