5ec1a4fef95723d8e686615b20ad7cc33b5c17a3
OrdinaryAzazel <19622052@mahasiswa.itb.ac.id>
2023-09-26 23:41:59 +0700

    fix: OPP

--- src/DriverMain.java
+++ src/DriverMain.java
@@ -1,8 +1,8 @@
 package src;
 
-public class DriverMain {
-    public static void main (String[] args){
-        Menu menu = new Menu();
-        menu.run();
-    }
-}
+// public class DriverMain {
+//     public static void main (String[] args){
+//         Menu menu = new Menu();
+//         menu.run();
+//     }
+// }
--- src/Matrix.java
+++ src/Matrix.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 
 public class Matrix {
 
-    public int row,col; //m itu baris,n itu kolom
-    public int rowMin=0;
-    public int colMin =0;
-    public double[][] matrix;//ax
-    public double[] b;//b
-    public double[] array;
-    public double[][] augmentedMatrix;
+    private int row,col; //m itu baris,n itu kolom
+    // private int rowMin=0;
+    // private int colMin =0;
+    private double[][] matrix;//ax
+    // private double[] b;//b
+    // private double[][] augmentedMatrix;
+    public static final double decPoint = 10000000000d;
     Scanner scanner = new Scanner(System.in);
     /* ********** KONSTRUKTOR ********** */
 
@@ -17,8 +17,9 @@ public class Matrix {
         this.row = baris;
         this.col = kolom;
         this.matrix = new double[baris][kolom];
-        this.augmentedMatrix = new double[baris][kolom+1];
-        this.b = new double[baris]; 
+        // this.augmentedMatrix = new double[baris][kolom+1];// gabungin di akhir operasi gauss
+        // this.b = new double[baris]; 
+        // createAugmentedMatrix();
     }
     
     public Matrix(double[][] matrix) {
@@ -35,23 +36,23 @@ public class Matrix {
         // this.b[i] = b[i];
     }
 }
-public void createAugmentedMatrix() //ngebuat augmented matrix dari matrix a dan persamaan b,ax=b
-{
-        for(int i = 0; i < row; i++)
-        {
-            for(int j = 0; j < col+1; j++)
-            {   
-                if (j == col)
-                {
-                    this.augmentedMatrix[i][j] = this.b[i];
-                }
-                else
-                {
-                    this.augmentedMatrix[i][j] = this.matrix[i][j];
-                }
-            }
-        }
-    }
+    // public void createAugmentedMatrix() //ngebuat augmented matrix dari matrix a dan persamaan b,ax=b
+    // {
+    //         for(int i = 0; i < row; i++)
+    //         {
+    //             for(int j = 0; j < col+1; j++)
+    //             {   
+    //                 if (j == col)
+    //                 {
+    //                     this.augmentedMatrix[i][j] = this.b[i];
+    //                 }
+    //                 else
+    //                 {
+    //                     this.augmentedMatrix[i][j] = this.matrix[i][j];
+    //                 }
+    //             }
+    //         }
+    //     }
     
     
     
@@ -67,60 +68,61 @@ public void createAugmentedMatrix() //ngebuat augmented matrix dari matrix a dan
         {
             for(int j = 0; j < this.col; j++)
             {   
-                System.out.println("Isi elemen a ke [" + (i) + "]" +"[" + (j) + "] : ");
+                System.out.println("Isi elemen ke [" + (i) + "]" +"[" + (j) + "] : ");
                 double elemen = scanner.nextDouble();
                 this.matrix[i][j] = elemen;//koef a[i][j]
                 
             }
-            System.out.println("Isi elemen b ke [" + (i) + "] : ");
-            double elemen = scanner.nextDouble(); //elemen b[i]
-            this.b[i]=elemen;
-            // scanner.close();
         }
-        // return matrix;
+           
+        // if (isAugmented)
+        // { 
+        //     for(int i = 0; i < this.row; i++)
+        //     {
+        //     System.out.println("Isi elemen b ke [" + (i) + "] : ");
+        //     double elemen = scanner.nextDouble(); //elemen b[i]
+        //     this.b[i]=elemen;
+        //     }
+        // }
+            
+        
+        // if (isAugmented)
+        // {
+        //     createAugmentedMatrix();
+        // }
+        System.out.print("\n");
     }
 
    
 
-    public void printMatrix(boolean isAugmented) {
-        if (!isAugmented){
-            for (int i = 0; i < this.row; i++) {
-                for (int j = 0; j < this.col; j++) {
-                    this.matrix[i][j]+=0; //menghilangkan -0
-                    System.out.format(" %.2f ", this.matrix[i][j]);
-                }
-    
-                System.out.println();
+    public void printMatrix() {
+        for (int i = 0; i < this.row; i++) {
+            for (int j = 0; j < this.col; j++) {
+                this.matrix[i][j]+=0; //menghilangkan -0
+                System.out.format(" %.2f ", this.matrix[i][j]);
             }
-        } else {
-            for (int i = 0; i < this.row; i++) {
-                for (int j = 0; j < this.col + 1; j++) {
-                    this.augmentedMatrix[i][j]+=0; //menghilangkan -0
-                    System.out.format(" %.2f ", this.augmentedMatrix[i][j]);
-                }
-    
-                System.out.println();
-            }
+
+            System.out.print("\n");
         }
-        System.out.println();
-    }
+        } 
+    
 
     /* ********** SELEKTOR ********** */
-    public int GetFirstIdxBrs(Matrix M) {
-        return 0;
-    }
+    // public int GetFirstIdxBrs(Matrix M) {
+    //     return 0;
+    // }
 
-    public int GetFirstIdxKol(Matrix M) {
-        return 0;
-    }
+    // public int GetFirstIdxKol(Matrix M) {
+    //     return 0;
+    // }
 
-    public int GetLastIdxBrs(Matrix M) {
-        return M.row - 1;
-    }
+    // public int GetLastIdxBrs(Matrix M) {
+    //     return M.row - 1;
+    // }
 
-    public int GetLastIdxKol(Matrix M) {
-        return M.col - 1;
-    }
+    // public int GetLastIdxKol(Matrix M) {
+    //     return M.col - 1;
+    // }
 
     // *** Operasi perkalian matriks ***
     public static Matrix multiple (Matrix m, double k){
@@ -143,10 +145,12 @@ public void createAugmentedMatrix() //ngebuat augmented matrix dari matrix a dan
         return M;
     }
 
+
+
     // Operasi Baris Elementor
     public void swap (int row1, int row2){
         double [] temp = matrix[row1];
-        matrix [row1] = matrix[2];
+        matrix [row1] = matrix[row2];
         matrix[row2] = temp;
     }
 
@@ -205,84 +209,101 @@ public void createAugmentedMatrix() //ngebuat augmented matrix dari matrix a dan
     public static void main(String args[])
     {
 
-        Matrix testSpl = new Matrix(3,3);
+        Matrix testSpl = new Matrix(3,4);
         testSpl.readMatrix();
-        testSpl.printMatrix(false);
-        testSpl.createAugmentedMatrix();
-        testSpl.printMatrix(true);
+        testSpl.printMatrix();
+        testSpl.eselonBaris();
+        System.out.println("\n");
+        testSpl.printMatrix();
 
 }
 
-public static void Copy(Matrix in, Matrix out){
-    out.row = in.row;
-    out.col = in.col;
-    out.matrix = new double[in.row][in.col];
-
-    for(int i=0; i<in.row;++i){
-        for(int j=0; j<in.col; ++j){
-            out.matrix[i][j] = in.matrix[i][j];
-        }
-    }
-}
-
-public static Matrix EliminasiGauss(Matrix M) {
+    public static void Copy(Matrix in, Matrix out){
+        out.row = in.row;
+        out.col = in.col;
+        out.matrix = new double[in.row][in.col];
 
 
-    // Inisialisasi
-    Matrix N = new Matrix(1, 1);
-    Copy(M,N);
-
-    // Proses mengurutkan baris
-    int[] zero = new int[N.row];
-    for (int i = 0; i < N.row; i++) { // Kalkulasi jumlah 0
-        zero[i] = 0;
-        int j = 0;
-        while (j < N.col && N.matrix[i][j] == 0) {
-            zero[i]++;
-            j++;
-        }
-    }
-    for (int i = 0; i < N.row; i++) { // Algoritma Pengurutan
-        for (int j = 0; j < N.row - 1; j++) {
-            if (zero[j] > zero[j + 1]) {
-                int temp;
-                N.Swap(j, j + 1);
-                temp = zero[j];
-                zero[j] = zero[j + 1];
-                zero[j + 1] = temp;
+        for(int i=0; i<in.row;++i){
+            for(int j=0; j<in.col; ++j){
+                out.matrix[i][j] = in.matrix[i][j];
             }
         }
     }
 
-    // Proses mereduksi baris
-    int indent = 0;
+    public void eselonBaris() {
 
-    for (int i = 0; i < N.row; i++) {
-        // Mencari sel bernilai
-        while (i + indent < N.col && N.matrix[i][i + indent] == 0) {
-            indent++;
+
+        // Inisialisasi
+        // Matrix N = new Matrix(1, 1);
+        // Copy(M,N);
+
+        // Proses mengurutkan baris
+        int[] zero = new int[this.row];
+        for (int i = 0; i < this.row; i++) { // Kalkulasi jumlah 0
+            zero[i] = 0;
+            int j = 0;
+            while (j < this.col && matrix[i][j] == 0) {
+                zero[i]++;
+                j++;
+            }
         }
 
-        if (i + indent < N.col) {
-            // Ubah angka depan jadi 1
-            N.KaliBaris(i, 1 / N.matrix[i][i + indent]);
-
-            // Pengurangan baris dibawahnya
-            for (int j = i + 1; j < N.row; j++) {
-                if (N.matrix[j][i + indent] != 0) {
-                    N.KaliBaris(j, 1 / N.matrix[j][i + indent]);
-                    N.MinusBaris(j, i);
+        for (int i = 0; i < this.row; i++) { // Algoritma Pengurutan
+            for (int j = 0; j < this.row - 1; j++) {
+                if (zero[j] > zero[j + 1]) {
+                    int temp;
+                    swap(j, j + 1);
+                    temp = zero[j];
+                    zero[j] = zero[j + 1];
+                    zero[j + 1] = temp;
                 }
             }
         }
+
+        // Proses mereduksi baris
+        int indent = 0;
+
+        for (int i = 0; i < this.row; i++) {
+            // Mencari sel bernilai
+            while (i + indent < this.col && this.matrix[i][i + indent] == 0) {
+                indent++;
+            }
+
+            if (i + indent < this.col) {
+                // Ubah angka depan jadi 1
+                kalirow(i, 1 / this.matrix[i][i + indent]);
+
+                // Pengurangan baris dibawahnya
+                for (int j = i + 1; j < this.row; j++) {
+                    if (this.matrix[j][i + indent] != 0) {
+                        kalirow(j, 1 / this.matrix[j][i + indent]);
+                        minusrow(j, i);
+                    }
+                }
+            }
+        }
+        approximate();
     }
-    N.Approximate();
+
+    public void approximate()
+    {
+        for (int i =0;i<row;i++)
+        {
+            for (int j =0 ; j<col;j++)
+            {
+                matrix[i][j]=(Math.round(matrix[i][j] * decPoint)/decPoint);
+            }
+        }
+    }
 
 
 
 
 
 
-}
+
+
+
 }
 
