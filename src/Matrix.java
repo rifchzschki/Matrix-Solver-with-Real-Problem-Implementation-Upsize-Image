05220e3432ed74811579ef01d124bc2b27de0d1e
cupski <yusufrafi77@gmail.com>
2023-09-27 19:21:38 +0700

    Update Matrix.java

--- src/Matrix.java
+++ src/Matrix.java
@@ -247,21 +247,22 @@ public class Matrix {
 
         Matrix testSpl = new Matrix(3,4);
         Matrix A = new Matrix(testSpl);
+        testSpl.interpolasiGaus(3);
         
-        SPL tes = new SPL();
-        testSpl.readMatrix();
-        A.Copy(testSpl);
-        testSpl.printMatrix();
-        A.printMatrix();
-        tes.getsolustioncramer(testSpl, A);
-        A = A.subcramer(testSpl, 0);
-        A.printMatrix();
-        testSpl.eselonBaris();
-        System.out.println("\n");
-        testSpl.printMatrix();
-        testSpl.eselonBarisReduksi();
-        System.out.println("\n");
-        testSpl.printMatrix();
+        // SPL tes = new SPL();
+        // testSpl.readMatrix();
+        // A.Copy(testSpl);
+        // testSpl.printMatrix();
+        // A.printMatrix();
+        // tes.getsolustioncramer(testSpl, A);
+        // A = A.subcramer(testSpl, 0);
+        // A.printMatrix();
+        // testSpl.eselonBaris();
+        // System.out.println("\n");
+        // testSpl.printMatrix();
+        // testSpl.eselonBarisReduksi();
+        // System.out.println("\n");
+        // testSpl.printMatrix();
 
     }
 
@@ -288,7 +289,6 @@ public class Matrix {
                 }
             }
         }
-
         Matrix N = new Matrix(m);
         N.Copy(m);
         N.col -= 1;
@@ -511,6 +511,99 @@ public class Matrix {
 
     }
 
+
+    public void interpolasiGaus(int N)
+    {
+        Matrix point = new Matrix(N,2);//masukan nilai x,y pada tiap titik
+        for (int i =0;i<N;i++)
+        {
+            for (int j =0;j<2;j++)
+            {
+                if (j==0)
+                {
+                    System.out.print("X"+i+": ");
+                }
+                else
+                {
+                    System.out.print("Y"+i+": ");
+                }
+                point.matrix[i][j]=scanner.nextDouble();
+            }
+        }
+
+        Matrix equation =new Matrix(N,N+1);//masukan dalam matrix untuk eliminasi gauss jordan
+        int power;
+        for(int i =0;i<N;i++)
+        {   
+            power =1;
+            for(int j =0;j<N+1;j++)
+            {
+                if (j==0)
+                {
+                    equation.matrix[i][j]=1;
+                }
+                else if (j==N)
+                {
+                    equation.matrix[i][j]=point.matrix[i][1];
+                }
+                else
+                {
+                    equation.matrix[i][j]=pow(point.matrix[i][0],power);
+                    power++;
+                }
+            }
+        }
+
+        //langkah gaus jordan
+        equation.eselonBarisReduksi();
+
+        //simpan hasil solusi
+
+        double[] solution= new double[N];
+         // Penyelesaian balik (back substitution)
+         for (int i = N - 1; i >= 0; i--) {
+             solution[i] = equation.matrix[i][N];
+             for (int j = i + 1; j < N; j++) {
+                 solution[i] -= equation.matrix[i][j] * solution[j];
+             }
+             solution[i] /= equation.matrix[i][i];
+         }
+ 
+        
+         System.out.print("X"+N+": ");
+         double X = scanner.nextDouble();
+         double Y = 0;
+         power =1;
+         for (int i =1;i<N;i++)
+         {
+            Y+=solution[i]*pow(X,power);
+            power++;
+         }
+         Y+=solution[0];
+         System.out.print("Y"+N+": " + Y);
+
+
+
+
+
+
+         
+    }
+
+    public double pow(double x,int y)
+    {
+        
+        if (y==0)
+        {
+            return 1;
+
+        }
+        else
+        {
+            return (x*pow(x,y-1));
+        }
+    
+    }
 
 
 
