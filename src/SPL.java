9770ca12b1c1b090e8916c008aacdc81205b4353
cupski <yusufrafi77@gmail.com>
2023-09-28 02:25:07 +0700

    update OBE determinant

--- src/SPL.java
+++ src/SPL.java
@@ -89,20 +89,20 @@ public class SPL {
 
     public void OBEdeterminant(Matrix m){
         double diagonal = 1;
-        double det =0;
+        double det =1;
         double p = m.pow((-1), m.matriksSegitigaAtas());
+        double epsilon = 1e-10; // Toleransi yang sangat kecil
         for (int i =0; i<= m.GetLastIdxBrs();i++){
             for(int j =0; j<= m.GetLastIdxKol();j++){
                 if (i == j){
-                    if (m.GetElmt(i, j) == 0 || m.GetElmt(i, j) == -0){
-                        diagonal =0;
+                    if (Math.abs(m.GetElmt(i, j)) < epsilon) {
+                        diagonal = 0.0; // Jika elemen sangat mendekati 0, dianggap sebagai 0
                     }
                     diagonal *= m.GetElmt(i, j);
                 }
             }
         }
-
-        det = (p*diagonal);
+        det = (p*diagonal)+0;
         System.out.format("%.2f", det);
     }
     
