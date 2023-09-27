9c118cda37d6c959019108e11239fb95ccad86c9
rifchzschki <114592995+rifchzschki@users.noreply.github.com>
2023-09-28 04:05:33 +0700

    new file:   src/Interpolasi.java 	modified:   src/Matrix.java

--- src/Matrix.java
+++ src/Matrix.java
@@ -81,6 +81,11 @@ public class Matrix {
         }
     } 
     
+    /* ********** SETTER ********** */
+    public void setEl(int i, int j, double val){
+        this.matrix[i][j] = val;
+    }
+
 
     /* ********** SELEKTOR ********** */
     // public int GetFirstIdxBrs(Matrix M) {
@@ -94,7 +99,7 @@ public class Matrix {
     public double[] GetRow(int row){
         return this.matrix[row];
     }
-    
+
     public double GetElmt(int i, int j){
         return this.matrix[i][j];
     }
@@ -557,89 +562,7 @@ public class Matrix {
     
 
 
-    public void interpolasiGaus(int N)
-    {
-        Matrix point = new Matrix(N,2);//masukan nilai x,y pada tiap titik
-        for (int i =0;i<N;i++)
-        {
-            for (int j =0;j<2;j++)
-            {
-                if (j==0)
-                {
-                    System.out.print("X"+i+": ");
-                }
-                else
-                {
-                    System.out.print("Y"+i+": ");
-                }
-                point.matrix[i][j]=scanner.nextDouble();
-            }
-        }
-
-        Matrix equation =new Matrix(N,N+1);//masukan dalam matrix untuk eliminasi gauss jordan
-        int power;
-        for(int i =0;i<N;i++)
-        {   
-            power =1;
-            for(int j =0;j<N+1;j++)
-            {
-                if (j==0)
-                {
-                    equation.matrix[i][j]=1;
-                }
-                else if (j==N)
-                {
-                    equation.matrix[i][j]=point.matrix[i][1];
-                }
-                else
-                {
-                    equation.matrix[i][j]=pow(point.matrix[i][0],power);
-                    power++;
-                }
-            }
-        }
-
-        //langkah gaus jordan
-        equation.eselonBarisReduksi();
-
-        //simpan hasil solusi
-
-        double[] solution= new double[N];
-         // Penyelesaian balik (back substitution)
-         for (int i = N - 1; i >= 0; i--) {
-             solution[i] = equation.matrix[i][N];
-             for (int j = i + 1; j < N; j++) {
-                 solution[i] -= equation.matrix[i][j] * solution[j];
-             }
-             solution[i] /= equation.matrix[i][i];
-         }
-         System.out.print("P(X)= ("+solution[0]+")A0");
-         
-         for (int i =1;i<N;i++)
-         {
-            System.out.print("+("+solution[i]+")A"+i);
-         }
-         
-         System.out.print("X"+N+": ");
-         double X = scanner.nextDouble();
-         double Y = 0;
-         power =1;
-         for (int i =1;i<N;i++)
-         {
-            Y+=solution[i]*pow(X,power);
-            power++;
-         }
-         Y+=solution[0];
-         System.out.print("Y"+N+": " + Y);
-
-
-
-
-
-
-         
-    }
-
+    
     public double pow(double x,double y)
     {
         
